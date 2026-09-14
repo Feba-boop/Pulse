bb4b668ca04b23bbd59f069d1b406437df9926d1
Feba-boop <s.biernacki23@gmail.com>
2026-09-14 18:09:11 +0200

    Add news filtering, sorting and result limit

--- index.js
+++ index.js
@@ -1,34 +1,73 @@
 async function pobierzNewsy() {
   try {
-    let response = await fetch("https://api.currentsapi.services/v1/latest-news", {
-    headers: {
-      Authorization: process.env.CURRENTS_API_KEY
-    }
-  });
-    
-    if (!response.ok) {
-        if (response.status === 401) {
-          console.log("Problem z kluczem API");
-        }else if (response.status === 404) {
-          console.log("Nie znaleziono endpointu");
-        }else {
-          console.log("Inny błąd API");
-        }
-        return;
-    }
-  
-    let data = await response.json();
-        console.log(data);
+    let response = await fetch(
+      "https://api.currentsapi.services/v1/latest-news",
+      {
+        headers: {
+          Authorization: process.env.CURRENTS_API_KEY,
+        },
+      },
+    );
 
-  for (let i = 0; i < 5 && i < data.news.length; i++) {
-    console.log("Tytuł: " + data.news[i].title);
-    console.log("Data: " + data.news[i].published);
-    console.log("Link: " + data.news[i].url);
-    console.log()
-}
+    if (!response.ok) {
+      if (response.status === 401) {
+        console.log("Problem z kluczem API");
+      } else if (response.status === 404) {
+        console.log("Nie znaleziono endpointu");
+      } else {
+        console.log("Inny błąd API");
+      }
+      return;
+    }
+
+    let data = await response.json();
+
+    let prosteNewsy = data.news.map(function (news) {
+      return {
+        title: news.title,
+        published: news.published,
+        url: news.url,
+        author: news.author,
+        language: news.language,
+        category: news.category,
+      };
+    });
+
+    let angielskieNewsy = filtrujPoJezyku(prosteNewsy, "en");
+    let generalneAngielskieNewsy = filtrujPoKategorii(
+      angielskieNewsy,
+      "general",
+    );
+
+    let posortowaneNewsy = sortujPoDacie(generalneAngielskieNewsy);
+    let pierwsze5 = ograniczLiczbeNewsow(posortowaneNewsy, 5);
+
+    console.log(pierwsze5);
   } catch (error) {
     console.log(error);
+  }
 }
+
+function filtrujPoJezyku(newsy, jezyk) {
+  return newsy.filter(function (news) {
+    return news.language === jezyk;
+  });
+}
+
+function filtrujPoKategorii(newsy, kategoria) {
+  return newsy.filter(function (news) {
+    return news.category.includes(kategoria);
+  });
+}
+
+function sortujPoDacie(newsy) {
+  return newsy.sort(function (a, b) {
+    return new Date(b.published) - new Date(a.published);
+  });
+}
+
+function ograniczLiczbeNewsow(newsy, limit) {
+  return newsy.slice(0, limit);
 }
 
 pobierzNewsy();
